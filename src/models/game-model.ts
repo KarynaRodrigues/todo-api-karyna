45377cdc5358c131d347b8ca515e331ced8fe889
KarynaRodrigues <160973700+KarynaRodrigues@users.noreply.github.com>
2024-05-29 16:58:21 -0300

    Update and rename game-model.ts to movie-model.ts

--- src/models/game-model.ts
+++
@@ -1,8 +0,0 @@
-export interface Game {
-    id: number;
-    title: string;
-    description: string;
-    releaseDate: Date;
-    createAt: Date;
-    updateAt: Date;
-}
---
+++ src/models/movie-model.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+    id: number
+    title: string
+    director: string
+    releaseYear: number
+    createdAt: Date
+    updatedAt: Date
+}
